62303fe577fa5e6f91dc89dcd5f7bee610eee655
Zhou ZhaoPing(智能媒体研发中心) <zhaopingzhou@sohu-inc.com>
2018-08-02 15:31:25 +0800

    server struct

---
+++ backend/server/src/server/server.go
@@ -0,0 +1,96 @@
+package server
+
+import (
+	"github.com/kataras/iris"
+
+	"github.com/kataras/iris/middleware/logger"
+	"github.com/kataras/iris/middleware/recover"
+
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+	"io/ioutil"
+	"strconv"
+)
+
+func main() {
+	if pid := syscall.Getpid(); pid != 1 {
+		ioutil.WriteFile("server.pid", []byte(strconv.Itoa(pid)), 0777)
+	}
+	//创建监听退出chan
+	c := make(chan os.Signal)
+	//监听指定信号 ctrl+c kill
+	signal.Notify(c, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT) //, syscall.SIGUSR1, syscall.SIGUSR2)
+	go func() {
+		for s := range c {
+			switch s {
+			case syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT:
+				fmt.Println("退出", s)
+				ExitFunc()
+			case syscall.SIGHUP:
+				fmt.Println("sighup", s)
+				//case syscall.SIGUSR1:
+				//    fmt.Println("usr1", s)
+				//case syscall.SIGUSR2:
+				//    fmt.Println("usr2", s)
+			default:
+				fmt.Println("other", s)
+			}
+		}
+	}()
+
+	fmt.Println("进程启动...")
+	go func() {
+		sum := 0
+		for {
+			sum++
+			fmt.Println("sum:", sum)
+			time.Sleep(time.Second)
+		}
+	}()
+
+	app := iris.New()
+	app.Logger().SetLevel("debug")
+	// Optionally, add two built'n handlers
+	// that can recover from any http-relative panics
+	// and log the requests to the terminal.
+	app.Use(recover.New())
+	app.Use(logger.New())
+
+	// Method:   GET
+	// Resource: http://localhost:8080
+	app.Handle("GET", "/", func(ctx iris.Context) {
+		ctx.HTML("<h1>Welcome</h1>")
+	})
+
+	// same as app.Handle("GET", "/ping", [...])
+	// Method:   GET
+	// Resource: http://localhost:8080/ping
+	app.Get("/ping", func(ctx iris.Context) {
+		ctx.WriteString("pong")
+	})
+
+	// Method:   GET
+	// Resource: http://localhost:8080/hello
+	app.Get("/hello", func(ctx iris.Context) {
+		ctx.JSON(iris.Map{"message": "Hello Iris!"})
+	})
+
+	// http://localhost:8080
+	// http://localhost:8080/ping
+	// http://localhost:8080/hello
+	app.Run(iris.Addr(":8080"), iris.WithoutServerError(iris.ErrServerClosed))
+
+	//<-sigTERM
+	//log.Print("killed")
+}
+
+func ExitFunc() {
+	fmt.Println("开始退出...")
+	fmt.Println("执行清理...")
+	os.Remove("server.pid")
+	fmt.Println("结束退出...")
+	os.Exit(0)
+}
