1286597307411560856ef2a9e41d45403b349ff2
zhouzhaoping <zhouzhaoping11@gmail.com>
2018-08-07 03:34:01 +0800

    test go using python

--- backend/server/src/handler/informarion_spider.go
+++ backend/server/src/handler/informarion_spider.go
@@ -10,6 +10,7 @@ func Refresh(star_id int) {
 	lasttime := findLastTime(star_id)
 	if lasttime == nil{
 		cmd := exec.Command("ls")
+		cmd = exec.Command("python", "../backend/informarion_spider/starspider.py")
 		buf, err := cmd.Output()
 		fmt.Printf("%s\n%s",buf,err)
 	}else{
