0b3ea7fb7fe046999da6a74e1a6cb57a6e5cb92d
zhouzhaoping <zhouzhaoping11@gmail.com>
2018-08-26 16:55:06 +0800

    fix cors again

--- backend/server/src/webtool/cors.go
+++ backend/server/src/webtool/cors.go
@@ -1,10 +1,21 @@
 package webtool
 
-import "github.com/kataras/iris"
+import (
+	"github.com/kataras/iris"
+	"strings"
+)
 
 func SetCros(ctx iris.Context){
-	ctx.ResponseWriter().Header().Set("Access-Control-Allow-Origin", "*")//允许访问所有域
-	ctx.ResponseWriter().Header().Add("Access-Control-Allow-Methods", "POST,GET,OPTIONS,PUT")
-	ctx.ResponseWriter().Header().Add("Access-Control-Allow-Headers","Content-Type")//header的类型
-	ctx.ResponseWriter().Header().Set("content-type","application/json")//返回数据格式是json
+	Origin := ctx.Request().Header.Get("Origin")
+	if 0 != len(Origin) && strings.Contains(Origin, "domain.com"){
+		ctx.ResponseWriter().Header().Add("Access-Control-Allow-Origin", Origin)
+		ctx.ResponseWriter().Header().Add("Access-Control-Allow-Methods", "POST,GET,OPTIONS,DELETE,PUT")
+		ctx.ResponseWriter().Header().Add("Access-Control-Allow-Headers", "x-requested-with,content-type")
+		ctx.ResponseWriter().Header().Add("Access-Control-Allow-Credentials", "true")
+		ctx.ResponseWriter().Header().Add("content-type","application/json")//返回数据格式是json
+	}
+	//ctx.ResponseWriter().Header().Set("Access-Control-Allow-Origin", "*")//允许访问所有域
+	//ctx.ResponseWriter().Header().Add("Access-Control-Allow-Methods", "POST,GET,OPTIONS,PUT")
+	//ctx.ResponseWriter().Header().Add("Access-Control-Allow-Headers","Content-Type")//header的类型
+	//ctx.ResponseWriter().Header().Set("content-type","application/json")//返回数据格式是json
 }
