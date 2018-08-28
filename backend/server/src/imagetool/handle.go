47e78fee571a80f34070b43f7bd2ce5349064d7e
zhouzhaoping <zhouzhaoping11@gmail.com>
2018-08-28 16:48:25 +0800

    test img and state

--- backend/server/src/imagetool/handle.go
+++ backend/server/src/imagetool/handle.go
@@ -28,7 +28,8 @@ func UploadHandler(ctx iris.Context) {
 	buff := make([]byte, 512)
 	_, err = file.Read(buff)
 	if err != nil {
-		log.Println(err)
+		log.Println(err, "readfile")
+		fmt.Println("readfile")
 		ctx.JSON(iris.Map{
 			"state": "Error:Upload Error - readfile.",
 		})
